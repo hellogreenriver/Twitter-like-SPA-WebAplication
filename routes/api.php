bfb9f47e961f306a8d2058fc372d65df44e0b6b2
hellogreenriver <maqatibet@hotmail.co.jp>
2021-11-01 12:46:42 +0900

    web.phpとapi.phpを編集

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+Route::get('/tomotter' , 'App\Http\Controllers\ApiController@index');
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', function(Request $request){
+        return $request->user();
+    });
+    Route::get('/individual' , 'App\Http\Controllers\ApiController@personalTweet' );
+    Route::post('/tweet', 'App\Http\Controllers\ApiController@create');
+    Route::delete('/delete/{id}', 'App\Http\Controllers\ApiController@delete');
+    Route::get('/search', 'App\Http\Controllers\ApiController@search');
+    Route::post('/logout', 'App\Http\Controllers\LoginController@logout');
 });
+
+Route::post('/login', 'App\Http\Controllers\LoginController@login');
+Route::post('/register','App\Http\Controllers\RegisterController@register');
+
+
